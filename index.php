0a6c121abb5f2913f48459a6aaea2b08828e65a6
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-22 11:04:47 -0300

    Basic HTML code for index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>PDO - Read Records</title>
+	<!--Bootstrap CSS-->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<!--Style CSS-->
+	<link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+
+	<div class="container">
+
+		<div class="page-header">
+			<h1>Lista de Produtos</h1>
+		</div>
+
+	</div>
+
+
+<!--jQuery-->
+<script src="js/jquery-3.2.1.min.js"></script>
+<!--JavaScript-->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
