08b01211f76c58eda8ccc5825a8626ade108f646
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-23 10:27:11 -0300

    Basic HTML code for update.php

---
+++ update.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>PDO - Update a Record</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<!-- Style CSS -->
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+	<div class="container">
+	
+		<div class="page-header">
+			<h1>Atualizar Produto</h1>
+		</div>
+
+	</div>
+	
+<!--jQuery-->
+<script src="js/jquery-3.2.1.min.js"></script>
+<!--JavaScript-->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
