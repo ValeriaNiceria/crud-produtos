b5a8b5d02d8191d0d3312df789b380382744fc80
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-23 10:41:52 -0300

    Read a record by ID parameter

--- update.php
+++ update.php
@@ -16,6 +16,40 @@
 			<h1>Atualizar Produto</h1>
 		</div>
 
+		<!-- Code PHP -->
+		<?php
+		//get passed parameter value, in this case, the record ID
+		$id = isset($_GET['id']) ? $_GET['id'] : die('ERRO: Não foi encontrado nenhum registro com esse ID.');
+
+		//include database connection
+		require_once("config/database.php");
+
+		//read current record's data
+		try {
+			//prepare selection query
+			$query = ("SELECT id, nome, descricao, preco FROM produtos WHERE id = :ID LIMIT 0,1");
+			$stmt = $conexao->prepare($query);
+
+			//this is the first question mark
+			$stmt->bindParam(':ID', $id);
+
+			//execute our query
+			$stmt->execute();
+
+			//store retrieved row to a variable
+			$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+			//values to fill up the form
+			$nome = $row['nome'];
+			$descricao = $row['descricao'];
+			$preco = $row['preco'];
+		} 
+		//show error
+		catch (PDOException $exception) {
+			die('Erro: ' . $exception->getMessage());
+		}
+		?>
+
 	</div>
 	
 <!--jQuery-->
