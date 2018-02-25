b8351947ba62f02f5d377580915c4b975f50e7f3
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-25 09:34:18 -0300

    PAGINATION IN PHP - Set pagination variables

--- index.php
+++ index.php
@@ -21,6 +21,18 @@
 		<?php
 		//include database connection
 		require_once("config/database.php");
+		
+
+		//PAGINATION VARIABLES
+		//page is the current page, if there's nothing set, default is page 1
+		$page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+		//set records or rows of data per page
+		$records_per_page = 5;
+
+		//calculate for the query LIMIT clause
+		$from_record_num = ($records_per_page * $page) - $records_per_page;
+
 
 		//delete message prompt
 		$action = isset($_GET['action']) ? $_GET['action'] : '';
