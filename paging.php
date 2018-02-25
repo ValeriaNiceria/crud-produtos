72fdb48cb951023d66fe6b2de6f3145dd7670847
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-25 09:52:02 -0300

    Create paging.php

---
+++ paging.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<ul class='pagination pull-left margin-zero mt0'>";
+
+//first page button will be here
+
+//clickable page numbers will be here
+
+//last page button will be here
+
+echo "</ul>";
+
+?>
