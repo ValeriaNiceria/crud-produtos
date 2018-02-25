f2be15fcbadd5d5dcff258b61e4660cc314fb0bd
ValeriaNiceria <valerianiceria@gmail.com>
2018-02-25 09:56:20 -0300

    Add first page button

--- paging.php
+++ paging.php
@@ -2,7 +2,15 @@
 
 echo "<ul class='pagination pull-left margin-zero mt0'>";
 
-//first page button will be here
+//first page button
+if ($page > 1) {
+	$prev_page = $page - 1;
+	echo "<li>";
+		echo "<a href='{$page_url}page={$prev_page}'>";
+			echo "<span style='margin:0 .5em;'> &laquo; </span>";
+		echo "</a>";
+	echo "</li>";
+}
 
 //clickable page numbers will be here
 
